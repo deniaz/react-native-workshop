4aaadd1d35c1d544e58e056cc38d5d449d64b63d
Robert Vogt <robert.vogt@namics.com>
2016-09-16 23:39:03 +0200

    Add basic styles

--- src/components/scenes/home.js
+++ src/components/scenes/home.js
@@ -2,16 +2,36 @@ import React from 'react';
 
 import { 
   View,
+  StyleSheet,
   Text,
 } from 'react-native';
 
 import CurrentState from '../../containers/current-state'
 
+const ALIZARIN = '#e74c3c';
+const EMERALD = '#2ecc71';
+
 const Home = (props) => (
-  <View>
-    <Text>SAC!</Text>
+  <View style={{ backgroundColor: props.isSynced ? EMERALD : ALIZARIN }, styles.container}>
+    <Text style={styles.title}>Tracker</Text>
+    <Text style={styles.subtitle}>Last Update 7 minutes ago</Text>
+
+    <View>
+      <Text>The Rest Data</Text>
+    </View>
     <CurrentState />
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+  title: {},
+  subtitle: {},
+  infoHeader: {},
+  infoData: {}
+});
 
 export default Home;
